74ebc2109c3cbd5ef97a558b97cc5d85f809cba0
Kadu Ribeiro <kaduesr@gmail.com>
2025-07-11 18:41:05 -0300

    Implementa CRUD para gerenciamento de clientes com validação e documentação Swagger

---
+++ src/main/java/com/kadudev/customerapi/dto/ClienteRequest.java
@@ -0,0 +1,27 @@
+package com.kadudev.customerapi.dto;
+
+import jakarta.validation.constraints.*;
+
+public record ClienteRequest(
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100, message = "O nome não pode ter mais de 100 caracteres")
+    String nome,
+
+    @NotBlank(message = "O CPF é obrigatório")
+    @Pattern(regexp = "\\d{11}", message = "O CPF deve conter 11 dígitos")
+    String cpf,
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
+    @Size(max = 100, message = "O e-mail não pode ter mais de 100 caracteres")
+    String email,
+
+    @NotBlank(message = "O telefone é obrigatório")
+    @Pattern(regexp = "\\d{10,11}", message = "O telefone deve conter 10 ou 11 dígitos")
+    String telefone,
+
+    @NotBlank(message = "O endereço é obrigatório")
+    @Size(max = 255, message = "O endereço não pode ter mais de 255 caracteres")
+    String endereco
+) {
+}
---
+++ src/main/java/com/kadudev/customerapi/model/Cliente.java
@@ -0,0 +1,44 @@
+package com.kadudev.customerapi.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(max = 100, message = "O nome não pode ter mais de 100 caracteres")
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "O CPF é obrigatório")
+    @Pattern(regexp = "\\d{11}", message = "O CPF deve conter 11 dígitos")
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
+    @Size(max = 100, message = "O e-mail não pode ter mais de 100 caracteres")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "O telefone é obrigatório")
+    @Pattern(regexp = "\\d{10,11}", message = "O telefone deve conter 10 ou 11 dígitos")
+    private String telefone;
+
+    @Column(nullable = false)
+    @NotBlank(message = "O endereço é obrigatório")
+    @Size(max = 255, message = "O endereço não pode ter mais de 255 caracteres")
+    private String endereco;
+
+}
