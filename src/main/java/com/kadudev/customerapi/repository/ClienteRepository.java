dd22994143361afa8d9df04598450f7e45b8a839
Kadu Ribeiro <kaduesr@gmail.com>
2025-07-11 18:50:28 -0300

    Ajusta formatação do código no ClienteRepository

--- src/main/java/com/kadudev/customerapi/repository/ClienteRepository.java
+++ src/main/java/com/kadudev/customerapi/repository/ClienteRepository.java
@@ -3,8 +3,9 @@ package com.kadudev.customerapi.repository;
 import com.kadudev.customerapi.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClienteRepository  extends JpaRepository<Cliente, Long>{
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     boolean existsByCpf(String cpf);
+
     boolean existsByEmail(String email);
-    
+
 }
